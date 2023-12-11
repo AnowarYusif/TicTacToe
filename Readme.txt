edf3e68d94dee2e0e87d52e51bdf45cd56ac1c39
tonyli9001 <tony.li@students.mq.edu.au>
2023-12-11 23:11:59 +0500

    Tic Tac Toe Project

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+Hi, I wanna tell you that it is very difficult to communicate to you here in Studypool
+what if we talk to whatsapp
+i think you have whatsapp account
+i m sharing my number 
+kindly message me
++923419648099
+
+
+i m sending you like this because of Studypool policy
--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  /*----- app's state (variables) -----*/
+  const [board, setBoard] = useState(Array(9).fill(''));
+  const [gameOver, setGameOver] = useState(false);
+  const [turn, setTurn] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  function handleTurn(event) {
+    let idx = event.target.id;
+
+    if (!gameOver && board[idx] === '') {
+      let newBoard = [...board];
+      newBoard[idx] = turn;
+      setBoard(newBoard);
+      setTurn(turn === 'X' ? 'O' : 'X');
+
+      const newWinner = getWinner(newBoard);
+
+      if (newWinner || newBoard.every((val) => val !== '')) {
+        setWinner(newWinner);
+        setGameOver(true);
+      }
+    }
+  }
+
+  function getWinner(board) {
+    for (let combo of winningCombos) {
+      const [a, b, c] = combo;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+
+    // Check for a tie
+    if (board.every((val) => val !== '')) {
+      return 'Tie';
+    }
+
+    return null;
+  }
+
+  function handleReset() {
+    setBoard(Array(9).fill(''));
+    setGameOver(false);
+    setTurn('X');
+    setWinner(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tic-React-Toe</h1>
+      <h2>{gameOver ? (winner ? `${winner === 'Tie' ? "It's a tie!" : `${winner} wins the game!`}` : "") : `It's ${turn}'s turn!`}</h2>
+      <div className="flex-container flex-column">
+        <div className="flex-container flex-wrap" id="board" onClick={handleTurn}>
+          {board.map((value, idx) => (
+            <div key={idx} className="square" id={idx}>
+              {value}
+            </div>
+          ))}
+        </div>
+        <button id="reset-button" onClick={handleReset}>
+          reset
+        </button>
+      </div>
     </div>
   );
 }
